ed385bd0495962a147f6ef97f2f423ef208e16c9
Florian  Valade <florian_val@outlook.fr>
2022-11-06 22:51:30 +0100

    format code

--- utils/mail.py
+++ utils/mail.py
@@ -13,22 +13,20 @@ import json
 
 
 class MailHandler(object):
- 
+
     __shared_instance = None
- 
+
     @staticmethod
     def getInstance():
- 
         """Static Access Method"""
         if MailHandler.__shared_instance == None:
             MailHandler()
         return MailHandler.__shared_instance
- 
+
     def __init__(self):
- 
         """virtual private constructor"""
         if MailHandler.__shared_instance != None:
-            raise Exception ("This class is a singleton class !")
+            raise Exception("This class is a singleton class !")
         else:
             MailHandler.__shared_instance = self
             self.__getConfig()
@@ -62,7 +60,8 @@ class MailHandler(object):
 
     def __getConfig(self):
         with open('config/config.yaml') as yaml_data_file:
-            self.config = yaml.load(yaml_data_file, Loader=yaml.FullLoader).get("Mail")
+            self.config = yaml.load(
+                yaml_data_file, Loader=yaml.FullLoader).get("Mail")
 
     @__usageMeasurement
     def __getDomainsList(self):
@@ -71,15 +70,17 @@ class MailHandler(object):
         headers = {
             'x-rapidapi-host': self.config.get("ApiHost"),
             'x-rapidapi-key': self.config.get("ApiKey")
-            }
+        }
 
         response = requests.request("GET", url, headers=headers)
 
         logging.info(f"Response : {response.status_code}, {response.text}")
 
-        domains_list = list(set(ast.literal_eval(response.text)) & set(self.config.get("TestedDomain")))
+        domains_list = list(set(ast.literal_eval(response.text))
+                            & set(self.config.get("TestedDomain")))
         if domains_list == [] or domains_list is None:
-            logging.error(f"No domain found and tested. falling back to non tested domains\n From Api : {response.text}, tested domains : {self.config.get('TestedDomain')}")
+            logging.error(
+                f"No domain found and tested. falling back to non tested domains\n From Api : {response.text}, tested domains : {self.config.get('TestedDomain')}")
             domains_list = list(set(ast.literal_eval(response.text)))
 
         self.domains = {"domains_list": domains_list,
@@ -91,15 +92,19 @@ class MailHandler(object):
         random_name = names.get_full_name().replace(" ", "_")
 
         self.__checkDomains()
-        mail_address = str(random_name + random.choice(self.domains["domains_list"])).lower()
+        mail_address = str(
+            random_name + random.choice(self.domains["domains_list"])).lower()
 
         if hasattr(self, "managed_address"):
-            self.managed_address.append({"mail_address":mail_address, "created": datetime.now()})
+            self.managed_address.append(
+                {"mail_address": mail_address, "created": datetime.now()})
         else:
-            self.managed_address = [{"mail_address":mail_address, "created": datetime.now()}]
+            self.managed_address = [
+                {"mail_address": mail_address, "created": datetime.now()}]
 
-        logging.info(f"Mail address created : {mail_address}") 
-        logging.info(f"Managed address : {list(map(lambda x:x.get('mail_address'), self.managed_address))}")
+        logging.info(f"Mail address created : {mail_address}")
+        logging.info(
+            f"Managed address : {list(map(lambda x:x.get('mail_address'), self.managed_address))}")
         self.__purgeManagedAddress()
         return mail_address
 
@@ -110,33 +115,38 @@ class MailHandler(object):
         if hasattr(self, "domains"):
             if "last_checked" in self.domains and (datetime.now() - self.domains.get("last_checked")).total_seconds() < self.config.get("DomainRenewalTime"):
                 return True
-    
+
         self.__getDomainsList()
         return True
 
     @__usageMeasurement
     def __GetMailsFromInbox(self, mail_address):
 
-        url = self.config.get("ApiUrl") + f"/request/mail/id/{hashlib.md5(mail_address.encode()).hexdigest()}/"
+        url = self.config.get(
+            "ApiUrl") + f"/request/mail/id/{hashlib.md5(mail_address.encode()).hexdigest()}/"
 
         headers = {
             'x-rapidapi-host': self.config.get("ApiHost"),
             'x-rapidapi-key': self.config.get("ApiKey")
-            }
+        }
 
         response = requests.request("GET", url, headers=headers)
 
-        try :
-            index = list(map(lambda x: x.get("mail_address"), self.managed_address)).index(mail_address)
-            self.managed_address[index].update({"mails": ast.literal_eval(response.text)})
+        try:
+            index = list(map(lambda x: x.get("mail_address"),
+                         self.managed_address)).index(mail_address)
+            self.managed_address[index].update(
+                {"mails": ast.literal_eval(response.text)})
         except ValueError:
-            self.managed_address.append({"mail_address":mail_address, "mails": ast.literal_eval(response.text)})
+            self.managed_address.append(
+                {"mail_address": mail_address, "mails": ast.literal_eval(response.text)})
 
-        logging.info(f'Get mails : {url} from : {mail_address} Response : {response.status_code}, number of message : {len(ast.literal_eval(response.text)) if type(ast.literal_eval(response.text)) is list else 0}')
+        logging.info(
+            f'Get mails : {url} from : {mail_address} Response : {response.status_code}, number of message : {len(ast.literal_eval(response.text)) if type(ast.literal_eval(response.text)) is list else 0}')
 
         return "GetMails"
 
-    def WaitForMail(self, mail_address, max_retry = 3):
+    def WaitForMail(self, mail_address, max_retry=3):
         max_retry = self.config.get("MaxRetry", max_retry)
         retry = 0
         last_mail = self.GetLastMail(mail_address)
@@ -150,7 +160,7 @@ class MailHandler(object):
             raise TimeoutError("No email found in inbox")
         self.ApproveDomain(mail_address)
         return last_mail
-    
+
     def _RemoveDuplicates(self, list_to_check):
         return list(set(list_to_check))
 
@@ -186,13 +196,15 @@ class MailHandler(object):
 
         self.__GetMailsFromInbox(mail_address)
 
-        index = list(map(lambda x: x.get("mail_address"), self.managed_address)).index(mail_address)
+        index = list(map(lambda x: x.get("mail_address"),
+                     self.managed_address)).index(mail_address)
         if type(self.managed_address[index].get("mails")) is dict and len(list(self.managed_address[index].get("mails").keys())) == 1:
             # No mail in box
             logging.error("No mail in inbox")
             return None
         elif type(self.managed_address[index].get("mails")) is list:
-            logging.debug(f"mail returned : {self.managed_address[index].get('mails')[-1]}")
+            logging.debug(
+                f"mail returned : {self.managed_address[index].get('mails')[-1]}")
             return self.managed_address[index].get("mails")[-1]
 
     def HtmlFromMail(self, mail):
@@ -205,7 +217,7 @@ class MailHandler(object):
     def __purgeManagedAddress(self):
         for index, address in enumerate(self.managed_address):
             if hasattr(address, "created") and (datetime.now() - address.get("created")).total_seconds() > self.config.get("AddressHandlingTime"):
-                
+
                 if self.config.get("DeleteMailOnDrop") and hasattr(address, "mails"):
                     for mail in address.get("mails"):
                         try:
@@ -213,11 +225,12 @@ class MailHandler(object):
                             self.__deleteMail(mail_id)
                         except AttributeError:
                             logging.info("No mail to delete")
-                
+
                 self.managed_address.remove(address)
 
-            if not "created" in address:
-                logging.error(f"{address.get('mail_address')} has no creation time")
+            if "created" not in address:
+                logging.error(
+                    f"{address.get('mail_address')} has no creation time")
                 self.managed_address[index]["created"] = datetime.now()
 
     @__usageMeasurement
@@ -227,7 +240,7 @@ class MailHandler(object):
         headers = {
             'x-rapidapi-host': self.config.get("ApiHost"),
             'x-rapidapi-key': self.config.get("ApiKey")
-            }
+        }
 
         response = requests.request("GET", url, headers=headers)
 
--- utils/web.py
+++ utils/web.py
@@ -6,33 +6,32 @@ import yaml
 import random
 import logging
 
+
 class webScrap(object):
 
     __shared_instance = None
- 
+
     @staticmethod
     def getInstance():
- 
         """Static Access Method"""
         if webScrap.__shared_instance == None:
             webScrap()
         return webScrap.__shared_instance
 
     def __init__(self):
-     
         """virtual private constructor"""
         if webScrap.__shared_instance != None:
-            raise Exception ("This class is a singleton class !")
+            raise Exception("This class is a singleton class !")
         else:
             webScrap.__shared_instance = self
             self.__getConfig()
         self.__newSession()
-    
+
     def __getConfig(self):
         with open('config/config.yaml') as yaml_data_file:
-            self.config = yaml.load(yaml_data_file, Loader=yaml.FullLoader).get("Web")
+            self.config = yaml.load(
+                yaml_data_file, Loader=yaml.FullLoader).get("Web")
 
-    
     def __newSession(self):
         self.session = HTMLSession()
 
@@ -44,13 +43,13 @@ class webScrap(object):
         # res.html.render()
 
         soup = BeautifulSoup(res.html.html, "html.parser")
-        return soup.find_all("form") 
+        return soup.find_all("form")
 
     @staticmethod
     def __getFormDetails(form):
         """Returns the HTML details of a form,
         including action, method and list of form controls (inputs, etc)"""
-        
+
         details = {}
         # get the form action (requested URL)
         action = form.attrs.get("action").lower()
@@ -65,9 +64,10 @@ class webScrap(object):
             # get name attribute
             input_name = input_tag.attrs.get("name")
             # get the default value of that input tag
-            input_value =input_tag.attrs.get("value", "")
+            input_value = input_tag.attrs.get("value", "")
             # add everything to that list
-            inputs.append({"type": input_type, "name": input_name, "value": input_value})
+            inputs.append(
+                {"type": input_type, "name": input_name, "value": input_value})
         # put everything to the resulting dictionary
         details["action"] = action
         details["method"] = method
@@ -77,7 +77,7 @@ class webScrap(object):
     def submitForms(self, email, first_name=None, last_name=None):
 
         self.__newSession()
-        
+
         if type(self.config.get("Urls")) is list:
             url = random.choice(self.config.get("Urls"))
         else:
@@ -86,22 +86,23 @@ class webScrap(object):
         if first_name is None or last_name is None:
             first_name, last_name = self.getNameFromMail(email)
 
-        logging.info(f"Submit form to {url} with : {email}, first_name: {first_name}, last_name: {last_name}")
+        logging.info(
+            f"Submit form to {url} with : {email}, first_name: {first_name}, last_name: {last_name}")
 
         forms = self.__getAllForms(url)
- 
+
         self.__checkFormsDetails(forms)
-       
+
         if len(forms) == 1:
             form_details = self.__getFormDetails(forms[0])
-        else: 
+        else:
             raise Exception("Multiple Forms found on page")
 
         # the data body we want to submit
-        data = {self.config.get("Form").get("FirstName") : first_name,
-                self.config.get("Form").get("LastName") : last_name,
-                self.config.get("Form").get("Mail0") : email,
-                self.config.get("Form").get("Mail1") : email}
+        data = {self.config.get("Form").get("FirstName"): first_name,
+                self.config.get("Form").get("LastName"): last_name,
+                self.config.get("Form").get("Mail0"): email,
+                self.config.get("Form").get("Mail1"): email}
 
         for input_tag in form_details["inputs"]:
             if input_tag["type"] == "hidden" or input_tag["type"] == "submit":
@@ -122,21 +123,27 @@ class webScrap(object):
             form_details = self.__getFormDetails(form)
             logging.info(f"Action of form : {form_details['action']}")
             logging.info(f"Method of form : {form_details['method']}")
-            logging.info(f"Hidden inputs : {len([input for input in form_details['inputs'] if input['type'] == 'hidden'])}")
-            
-            inputs = [input for input in form_details["inputs"] if input["type"] != 'hidden' and input["type"] != "submit"]
+            logging.info(
+                f"Hidden inputs : {len([input for input in form_details['inputs'] if input['type'] == 'hidden'])}")
+
+            inputs = [input for input in form_details["inputs"]
+                      if input["type"] != 'hidden' and input["type"] != "submit"]
 
             name_in_conf = list(self.config.get("Form").values())
-            inputs_name = list(map(lambda x:x["name"], inputs))
+            inputs_name = list(map(lambda x: x["name"], inputs))
             if len(set(name_in_conf) - set(inputs_name)) != 0:
-                logging.warning(f"Name in Form conf not found in inputs : {set(name_in_conf) - set(inputs_name)}")
+                logging.warning(
+                    f"Name in Form conf not found in inputs : {set(name_in_conf) - set(inputs_name)}")
             if len(set(inputs_name) - set(name_in_conf)) != 0:
-                logging.warning(f"Name in inputs not found in conf : {set(inputs_name) - set(name_in_conf)}")
+                logging.warning(
+                    f"Name in inputs not found in conf : {set(inputs_name) - set(name_in_conf)}")
 
     @staticmethod
     def getNameFromMail(email):
         try:
-            a, b = email.split("@")[0].split("_")[0], email.split("@")[0].split("_")[1]
+            a, b = email.split(
+                "@")[0].split("_")[0], email.split("@")[0].split("_")[1]
         except IndexError:
-            a, b = email.split("@")[0][:len(email.split("@")[0])//2], email.split("@")[0][len(email.split("@")[0])//2:]
+            a, b = email.split("@")[0][:len(email.split("@")[0]) //
+                                       2], email.split("@")[0][len(email.split("@")[0])//2:]
         return a, b
